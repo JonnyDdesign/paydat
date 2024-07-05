5b8e22b503e139e2a4a70d3ec4f387ecaa6de083
JonnyDdesign <jondesserres@gmail.com>
2024-07-05 12:45:15 -0700

    Chnged file name to server.go and

--- server.go
+++ server.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
 
-	var err error = http.ListenAndServe(addr:"localhost:4242", handler:nil)
+	var err error = http.ListenAndServe("localhost:4242", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(a...: "ENDPOINT CALLED!")
+	fmt.Println("ENDPOINT CALLED!")
 }
