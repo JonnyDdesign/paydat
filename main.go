af31b57dfda340fcb912c3a2f854c6eec3db121a
JonnyDdesign <jondesserres@gmail.com>
2024-07-04 22:19:55 -0700

    Added an if statement to check if an error occurs.

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
 
-	http.ListenAndServe(addr:"localhost:4242", handler:nil)
+	var err error = http.ListenAndServe(addr:"localhost:4242", handler:nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
