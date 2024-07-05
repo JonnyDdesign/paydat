5bac7958247a24708baf922c0b0a930a8017f000
JonnyDdesign <jondesserres@gmail.com>
2024-07-04 22:02:21 -0700

    Created a HandleFunc to request a connection to an endpoint. Also, created a handleCreatePaymentIntent that takes two parameters, a ResponseWriter and a pointer to the Request.

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(a...: "ENDPOINT CALLED!")
 }
