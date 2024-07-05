85a0a4bfc124c100d10c7a0604681f3478ce5d82
JonnyDdesign <jondesserres@gmail.com>
2024-07-04 22:09:57 -0700

    Created a ListeAndServe function to continuiously listen for requets.

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+
+	http.ListenAndServe(addr:"localhost:4242", handler:nil)
 }
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
